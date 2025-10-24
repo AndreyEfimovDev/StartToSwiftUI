47afbff730eff4708244ce7dd24bd8267687aec0
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-10-24 19:06:12 +0300

    Cleaning code

--- StartToSwiftUI/SFSymbolsAnimationView.swift
+++ StartToSwiftUI/SFSymbolsAnimationView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct _427_SFSymbolsView: View {
+struct SFSymbolsAnimationView: View {
     
     @State var valueToUse: Int = 0
     @State var isDeleted: Bool = false
@@ -61,5 +61,5 @@ struct _427_SFSymbolsView: View {
 }
 
 #Preview {
-    _427_SFSymbolsView()
+    SFSymbolsAnimationView()
 }
