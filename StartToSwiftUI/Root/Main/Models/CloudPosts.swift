7d3d27f934981a53cc5421d8ca749cec4670681e
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-10-24 13:58:12 +0300

    Cleaning code

--- StartToSwiftUI/Root/Main/Models/CloudPosts.swift
+++ StartToSwiftUI/Root/Main/Models/CloudPosts.swift
@@ -12,7 +12,7 @@ import Foundation
 // - to check and compare dates of Cloud posts and App posts for update purpose. If a date of Cloud posts is later than a date of App posts then a user will be notificated for updates on a HomeView when the App is lalunched or in the Preferences at the "Check posts for updates" button
 
 struct CloudPosts: Codable {
-    let lastUpdated: Date
+    let dateStamp: Date
     let cloudPosts: [Post]
     
 }
