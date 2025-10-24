eaf6b6b0f33e01c474036a0378eb964f984db25b
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-10-24 14:03:09 +0300

    Cleaning code

--- StartToSwiftUI/Root/Main/Models/CloudPosts.swift
+++ StartToSwiftUI/Root/Main/Models/CloudPosts.swift
@@ -8,11 +8,10 @@
 import Foundation
 
 // CloudPosts model is for:
-// - to download posts from Cloud as intermediate data massive between Cloud posts and App posts
+// - to keep posts remotely in the Cloud in this given format to track dateStamp for updates purposes
 // - to check and compare dates of Cloud posts and App posts for update purpose. If a date of Cloud posts is later than a date of App posts then a user will be notificated for updates on a HomeView when the App is lalunched or in the Preferences at the "Check posts for updates" button
 
 struct CloudPosts: Codable {
     let dateStamp: Date
     let cloudPosts: [Post]
-    
 }
