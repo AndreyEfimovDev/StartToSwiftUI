d561285215a4055a47412ec02f3ab7be9c18f169
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-10-27 15:09:51 +0300

    Checking for posts updates implementation

---
+++ StartToSwiftUI/Preferences/CheckForPostsUpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckForPostsUpdateView.swift
+//  StartToSwiftUI
+//
+//  Created by Andrey Efimov on 27.10.2025.
+//
+
+import SwiftUI
+
+struct CheckForPostsUpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckForPostsUpdateView()
+}
