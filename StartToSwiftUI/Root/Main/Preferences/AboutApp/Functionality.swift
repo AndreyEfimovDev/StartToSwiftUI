8664602cbf6b033b1f1c79ea5083fd7b1024626a
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-11-05 21:37:35 +0300

    Refactoring AboutApp

--- StartToSwiftUI/Root/Main/Preferences/AboutApp/Functionality.swift
+++ StartToSwiftUI/Root/Main/Preferences/AboutApp/Functionality.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct Functionality: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        • Organise learning resources by category.
+        ScrollView {
+            Text("""
+            The **StartToSwiftUI** application provides you with the following functionality:
+            
+            • Create personal collections of links to educational materials, add new and edit existing posts.
+            
+            • Download a curated collection of SwiftUI learning links composed by the developer from a cloud service upon your request.
+            
+            • Update a current collection of links from a cloud service upon your request when available.
+            
+            • Toggle a notification to be informed of updates to the curated collection from the developer.
+            
+            • Make a search though posts.
+            
+            • Navigate material by searching and filtering.
+            
+            • Backup, restore, share and erase posts. 
+
+            """)
+ 
+            .multilineTextAlignment(.leading)
+            .managingPostsTextFormater()
+            .padding(.horizontal)
+        }
+        .foregroundStyle(Color.mycolor.myAccent)
+        .navigationTitle("Functionality")
     }
 }
 
