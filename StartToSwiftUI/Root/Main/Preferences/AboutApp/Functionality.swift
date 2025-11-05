9036d2acb980b6eab46acac9731ee851d50f7a75
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-11-05 22:42:56 +0300

    Cleaning code

--- StartToSwiftUI/Root/Main/Preferences/AboutApp/Functionality.swift
+++ StartToSwiftUI/Root/Main/Preferences/AboutApp/Functionality.swift
@@ -18,10 +18,9 @@ struct Functionality: View {
             - Downloading a curated collection of links for learning SwiftUI composed by the developer from the cloud service upon your request.
             - Updating your current collection of links from a cloud service upon your request if updates are available.
             - Enabling notifications about the availability of updates to the developer's curated collection.
-            - Navigating through materials using search and filtering.
+            - Navigating through materials using search and filtering to quickly find and use the relevant information.
             - Backing up, restoring, sharing and deleting posts. 
             """)
- 
             .multilineTextAlignment(.leading)
             .managingPostsTextFormater()
             .padding(.horizontal)
