5fe93193d563d4c01b8b331a05f08628322e3a78
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-11-08 13:38:43 +0300

    Implementing Generic into FileManager funcs

--- StartToSwiftUI/Services/FileStorageService.swift
+++ StartToSwiftUI/Services/FileStorageService.swift
@@ -14,18 +14,18 @@ class FileStorageService: ObservableObject {
     
     let fileName = Constants.localFileName // "posts_app.json"
 
-    var fileURL: URL? {
-        guard
-            let path = FileManager
-                .default
-                .urls(for: .documentDirectory, in: .userDomainMask)
-                .first?
-                .appendingPathComponent(fileName) else {
-            print("❌ FM: Error getting path.")
-            return nil
-        }
-        return path
-    }
+//    var fileURL: URL? {
+//        guard
+//            let path = FileManager
+//                .default
+//                .urls(for: .documentDirectory, in: .userDomainMask)
+//                .first?
+//                .appendingPathComponent(fileName) else {
+//            print("❌ FM: Error getting path.")
+//            return nil
+//        }
+//        return path
+//    }
 
     // MARK: FILE MANAGER FUNCIONS
     
@@ -44,34 +44,42 @@ class FileStorageService: ObservableObject {
     }
     
     // Encoding posts and saving into JSON data
-    func savePosts(_ posts: [Post]) {
+    func savePosts<T: Codable>(
+        _ data: T,
+        filename: String,
+        encoder: JSONEncoder = .appEncoder // We use the date encoding strategy - ISO8601 (string)
+    ) {
         
-        guard let url = getFileURL(fileName: fileName) else { return }
+        guard let url = getFileURL(fileName: fileName) else {
+            print("❌ FM(savePosts): Error in getting url: \(filename)")
+            return
+        }
         
         do {
-            // We use the date encoding strategy to ISO8601 (string)
-            let jsonData = try JSONEncoder.appEncoder.encode(posts)
+            let jsonData = try encoder.encode(data)
             try jsonData.write(to: url)
-            print("✅ FM: Successfully saved в \(url)")
+            print("✅ FM(savePosts): Successfully saved в \(url)")
         } catch {
-            print("❌ FM: Error in saving posts: \(error)")
+            print("❌ FM(savePosts): Error in saving posts: \(error)")
         }
     }
     
     // Loading and decoding JSON data into posts
-    func loadPosts() -> [Post] {
+    func loadPosts<T: Codable>(
+        fileName: String,
+        encoder: JSONDecoder = .appDecoder // We use the date decoding strategy - ISO8601 (string)
+    ) -> T? {
         
-        guard let url = getFileURL(fileName: fileName) else { return [] }
+        guard let url = getFileURL(fileName: fileName) else { return nil }
         
         do {
-            let data = try Data(contentsOf: url)
-            // We use the date decoding strategy from ISO8601 (string)
-            let posts = try JSONDecoder.appDecoder.decode([Post].self, from: data)
-            print("✅ FM: Successfully uploaded \(posts.count) posts")
-            return posts
+            let jsonData = try Data(contentsOf: url)
+            let decodedData = try encoder.decode(T.self, from: jsonData)
+            print("✅ FM(loadPosts): Successfully uploaded")
+            return decodedData
         } catch {
-            print("☑️ FM: There are no saved posts on the device.: \(error)")
-            return []
+            print("☑️ FM(loadPosts): There are no saved posts on the device.: \(error)")
+            return nil
         }
     }
 }
