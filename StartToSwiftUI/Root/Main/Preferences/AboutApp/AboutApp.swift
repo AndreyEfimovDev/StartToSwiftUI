e802c080bce1272cb76b99485f8f3eaba52b69b4
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-11-14 19:20:36 +0300

    Notifications implementation
    plus refactoring AboutAppView

--- StartToSwiftUI/Root/Main/Preferences/AboutApp/AboutApp.swift
+++ StartToSwiftUI/Root/Main/Preferences/AboutApp/AboutApp.swift
@@ -14,52 +14,68 @@ struct AboutApp: View {
     
     var body: some View {
         
-        Form {
-            Section {
-                HStack {
-                    Text("Name")
-                    Spacer()
-                    Text("StartToSwiftUI")
+        ZStack {
+            VStack{
+                HStack (spacing: 0) {
+                    Image(systemName: "pencil.and.outline")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(Color.mycolor.myBlue)
+                        .padding()
+                    VStack (alignment: .leading) {
+                        Text("StartToSwiftUI")
+                            .font(.headline)
+                        Text("SwiftUI Hub Study")
+                            .font(.caption)
+                    }
                 }
-                HStack {
-                    Text("Version")
-                    Spacer()
-                    Text("01.01.01")
-                }
-                HStack {
-                    Text("Developed by")
-                    Spacer()
-                    Text("Andrey Efimov")
-                }
+                
+                Form {
+                    Section {
+                        HStack {
+                            Text("Version")
+                            Spacer()
+                            Text("01.01.01")
+                        }
+                        HStack {
+                            Text("Developed by")
+                            Spacer()
+                            Text("Andrey Efimov")
+                        }
+                    }
+                    .foregroundStyle(Color.mycolor.myAccent)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity)
+                    
+                    NavigationLink("Welcome") {
+                        WelcomeMessage()
+                    }
+                    .customPreferencesListRowStyle(
+                        iconName: "suit.heart",
+                        iconWidth: iconWidth
+                    )
+                    NavigationLink("Introduction") {
+                        Introduction()
+                    }
+                    .customPreferencesListRowStyle(
+                        iconName: "textformat.size.larger",
+                        iconWidth: iconWidth
+                    )
+                    NavigationLink("What's New") { //  list.bullet list.bullet.circle square.fill.text.grid.1x2
+                        WhatsNew()
+                    }
+                    .customPreferencesListRowStyle(
+                        iconName: "newspaper",
+                        iconWidth: iconWidth
+                    )
+                } // Form
+                .navigationTitle("About App")
+                .scrollContentBackground(.hidden)
+                .background(.clear)
             }
-            .foregroundStyle(Color.mycolor.myAccent)
-            .padding(.horizontal)
-            .frame(maxWidth: .infinity)
-
-            NavigationLink("Welcome") {
-                WelcomeMessage()
-            }
-            .customPreferencesListRowStyle(
-                iconName: "suit.heart",
-                iconWidth: iconWidth
-            )
-            NavigationLink("Introduction") {
-                Introduction()
-            }
-            .customPreferencesListRowStyle(
-                iconName: "textformat.size.larger",
-                iconWidth: iconWidth
-            )
-            NavigationLink("What's New") { //  list.bullet list.bullet.circle square.fill.text.grid.1x2
-                WhatsNew()
-            }
-            .customPreferencesListRowStyle(
-                iconName: "newspaper",
-                iconWidth: iconWidth
-            )
-        } // Form
+        } // ZStack
         .foregroundStyle(Color.mycolor.myAccent)
-        .navigationTitle("About App")
+        .background(.thickMaterial)
     }
 }
 
