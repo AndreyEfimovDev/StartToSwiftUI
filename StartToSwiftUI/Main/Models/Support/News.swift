43f21a7514a74fa1981b1c3989fdd81338c2e378
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2025-11-27 03:03:47 +0300

    Updating WhatsNews

--- StartToSwiftUI/Main/Models/Support/News.swift
+++ StartToSwiftUI/Main/Models/Support/News.swift
@@ -51,11 +51,16 @@ struct WhatsNews {
                 News(
                     title: "Initial deployment",
                     newsText: """
-                            The App's features are described in the 'Introduction/About App' section.
-                            """),
+                            StartToSwiftUI app features:
+                            - Personal Library: Create and manage your own collection of links to learning materials.
+                            - Curated Collection: Jumpstart your learning with a curated collection of SwiftUI tutorials and articles compiled from open sources. You will be notified when a new version of the collection is available for download. The developer strives to keep this collection up to date, though this cannot be guaranteed at all times.
+                            - Smart Organisation: Organise learning resources by category such as level of study, year of materials, type of source/media, etc, create a collection of favourite materials.
+                            - Full Control: Edit and delete your materials as needed, save drafts for further processing.
+                            - Efficient Search & Filter: Quickly find what you need using search and filtering tools.
+                            - Data Management: Backup, restore, share, or delete materials as needed.
+                            """
+                ),
             ]
         )
-
     ]
-    
 }
