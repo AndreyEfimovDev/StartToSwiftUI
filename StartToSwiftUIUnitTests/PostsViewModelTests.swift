935148ab662896e1612648d11f4dd316fa532eb9
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2026-01-21 20:44:42 +0300

    Adding tests for Models, VMs and utilities

---
+++ StartToSwiftUIUnitTests/PostsViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  PostsViewModelTests.swift
+//  StartToSwiftUIUnitTests
+//
+//  Created by Andrey Efimov on 21.01.2026.
+//
+
+import SwiftUI
+
+struct PostsViewModelTests: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostsViewModelTests()
+}
---
+++ StartToSwiftUIUnitTests/UtilityTests.swift
@@ -0,0 +1,18 @@
+//
+//  UtilityTests.swift
+//  StartToSwiftUIUnitTests
+//
+//  Created by Andrey Efimov on 21.01.2026.
+//
+
+import SwiftUI
+
+struct UtilityTests: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UtilityTests()
+}
