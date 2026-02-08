12fa0b607fa7fac66de684989d5609c91538d458
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2026-02-08 14:31:13 +0300

    Stroke around FiltersView for iPhone

--- StartToSwiftUI/Views/FiltersView.swift
+++ StartToSwiftUI/Views/FiltersView.swift
@@ -62,6 +62,19 @@ struct FiltersView: View {
         .foregroundStyle(Color.mycolor.myAccent)
         .padding(.top, -40)
         .padding(.horizontal)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(
+                    Color.mycolor.mySecondary,
+                    lineWidth: UIDevice.isiPhone ? 1 : 0
+                )
+                .mask(
+                    VStack {
+                        Rectangle().frame(height: 100)
+                        Spacer()
+                    }
+                )
+        )
         .onDisappear {
             vm.isFiltersEmpty = vm.checkIfAllFiltersAreEmpty()
             isFilterButtonPressed = false
