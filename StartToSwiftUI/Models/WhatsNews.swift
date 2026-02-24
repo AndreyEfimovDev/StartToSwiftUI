df60bd2cbe832998d1bcc5c5a65b0fc92a1a2efb
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2026-02-24 18:36:32 +0300

    Added news to Release 1.2.2(4)

--- StartToSwiftUI/Models/WhatsNews.swift
+++ StartToSwiftUI/Models/WhatsNews.swift
@@ -11,11 +11,12 @@ struct WhatsNews {
 #warning("Add upated info before deployment to App Store")
     static let releases: [Release] = [
         Release(
-            release: "Release 1.2.2(4)",
+            release: "Release 1.3.1(4)",
             news: [
                 News(
                     title: "Added",
                     newsText: """
+                    - Study content can be managed by first moving it to the 'hidden' pool, then to the 'deleted' pool, before it is permanently erased
                     - The "Random" sorting option in the filter to display materials in random order
                     """
                 ),
