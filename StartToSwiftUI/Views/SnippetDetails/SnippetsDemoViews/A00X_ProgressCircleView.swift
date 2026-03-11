e04e1e14dadbb08b0f7f961ae98e322ee5cf3cfe
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2026-03-11 10:40:26 +0300

    [Feature] Added A00X_ProgressCircleView

---
+++ StartToSwiftUI/Views/SnippetDetails/SnippetsDemoViews/A00X_ProgressCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  A00X_ProgressCircleView.swift
+//  StartToSwiftUI
+//
+//  Created by Andrey Efimov on 11.03.2026.
+//
+
+import SwiftUI
+
+struct A00X_ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    A00X_ProgressCircleView()
+}
